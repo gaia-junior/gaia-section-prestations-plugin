0346ad3656bb29e817a552ddca0672b454f09d3e
gaia-junior <77056366+gaia-junior@users.noreply.github.com>
2021-01-14 02:20:03 -0800

    first commit

---
+++ gaia-prestation-section.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Gaia section about plugin
+Plugin URI: https://github.com/gaia-junior/gaia-section-about-plugin
+Description: This is the plugin to design the section about into the gaia website
+Version: 0.1
+Author: Serge Mbele Amoungui
+Author URI: http://samoungui.com
+License: GPL2
+*/
